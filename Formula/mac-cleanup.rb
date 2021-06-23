ae80e3449ee639fca7dac7791b730690b1b6efd2
Florian Wartner <f.wartner@nexus-united.com>
2021-06-23 13:12:56 +0200

    Changes

--- Formula/mac-cleanup.rb
+++ Formula/mac-cleanup.rb
@@ -1,17 +1,16 @@
 class MacCleanup < Formula
     desc "🗑️ Cleanup script for OSX"
     homepage "https://wartner.io"
-    homepage "https://github.com/fwartner/homebrew-mac-cleanup"
     # revision "6b53c3877f194f32fe1bd2ad5fbc46b6426f5a79"
     version "1.0.6"
     license "MIT"
     head "https://github.com/fwartner/homebrew-mac-cleanup.git"
 
     def install
-      bin.install "mac-cleanup"
+        bin.install "mac-cleanup"
     end
 
     test do
-      assert_match "A Mac Cleanup Utility by fwartner", shell_output("#{bin}/mac-cleanup -h", 2)
+        assert_match "A Mac Cleanup Utility by fwartner", shell_output("#{bin}/mac-cleanup -h", 2)
     end
-  end
+end
