5ecc98bd707457386cc582b25f48a30747a242e0
Florian Wartner <fwartner@users.noreply.github.com>
2021-06-29 10:19:13 +0200

    mac-cleanup 1.1.4

--- Formula/mac-cleanup.rb
+++ Formula/mac-cleanup.rb
@@ -1,8 +1,8 @@
 class MacCleanup < Formula
   desc "🗑️ cleanup script for macos"
   homepage "https://github.com/fwartner/mac-cleanup"
-  url "https://github.com/fwartner/mac-cleanup/archive/v1.1.3.tar.gz"
-  sha256 "e164049ad56853cbe7180995c1c1bdc0f2849f7795fb1a369a83bef631934ae9"
+  url "https://github.com/fwartner/mac-cleanup/archive/v1.1.4.tar.gz"
+  sha256 "d8fbf6588571bf4beffa22c88526e13b23c71da52be90c777a1502b470028bd6"
   license "MIT"
 
   def install
